e11a02595513940b1d1b9afa5d9bbea304380a09
summerlove91 <gkdus3040@gmail.com>
2017-08-15 02:50:25 -0700

    Random Users created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,54 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'random_data'
+
+3.times do
+    User.create!(
+        name: "Standard User",
+        email: RandomData.random_email,
+        password: "password",
+        standard: true,
+        premium: false,
+        admin: false
+        )
+end
+
+2.times do
+    User.create!(
+        name: "Premiun User",
+        email: RandomData.random_email,
+        password: "password",
+        standard: false,
+        premium: true,
+        admin: false
+        )
+end
+
+2.times do
+    User.create!(
+        name: "Admin",
+        email: RandomData.random_email,
+        password: "password",
+        standard: false,
+        premium: false,
+        admin: true
+        )
+end
+
+users = User.all
+
+# 2.times do
+#     Wiki.create!(
+#         title: RandomData.random_sentence,
+#         body: RandomData.random_paragraph,
+#         # private: false
+#         )
+# end
+
+# wikis = Wiki.all
+
+puts "Seed finished"
+puts "#{users.count} users created"
+# puts "#{wikis.count} wikis created"
