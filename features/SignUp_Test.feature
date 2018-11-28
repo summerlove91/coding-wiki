691b107ee4db2a7457cb942de23a1c1118fd7755
summerlove91 <gkdus3040@gmail.com>
2018-11-27 22:39:58 -0800

    sign up step definition update

--- features/SignUp_Test.feature
+++ features/SignUp_Test.feature
@@ -1,36 +1,37 @@
 Feature: Testing for signup page
 
 Scenario: Standard User Signup with no user name
-  Given I want to use the browser Firefox
-  When I set the user name empty
-  When I set the user email 'test@test.com'
+  Given I navigate homepage using Firefox browser
+  And I click on Signup button and wait for the sign up page to display
+  When I set the username empty
+  When I set the user email test@test.com
   When I set the user password to password
   When I set the user password confirmation to password
-  Then I see the error message "Invalid email/password combination"
-  And I am on the login page
+  And I click on sign up button
+  Then I see the error message
 
 Scenario: Standard User Signup with bad email
-  Given I want to use the browser Firefox
-  When I set the user name to Test User
+  Given I am already on sign up page
+  When I set the username to Test User
   When I set the user email to dd
   When I set the user password to password
   When I set the user password confirmation to password
-  Then I see the error message "Invalid email/password combination"
-  And I am on the login page
+  And I click on sign up button
+  Then I see the error message
 
 Scenario: Standard User Signup with bad password
-  Given I want to use the browser Firefox
-  When I set the user name to Test User
+  Given I am already on sign up page
+  When I set the username to Test User
   When I set the user email to test@test.com
   When I set the user password to wrong
   When I set the user password confirmation to password
-  Then I see the error message "Invalid email/password combination"
-  And I am on the login page
+  And I click on sign up button
+  Then I see the error message
 
 Scenario: Standard User Signup with valid user email and password
-  Given I want to use the browser Firefox
-  When I set the user name to Test User
+  Given I am already on sign up page
+  When I set the username to Test User
   When I set the user email to test@test.com
   When I set the user password to password
   When I set the user password confirmation to password
-  Then I am on the homepage.
+  Then I am on the homepage
--- features/step_definitions/authentication/test_signup.rb
+++ features/step_definitions/authentication/test_signup.rb
@@ -1,123 +1,165 @@
-# require 'selenium-webdriver'
-#
-# driver = Selenium::WebDriver.for :firefox
-#
-# #Loading the assertselenium URL
-# driver.navigate.to "http://localhost:3000"
-#
-# ##SIGNUP WITH BAD USERNAME ######################
-# #find sign in button and click to navigate to sign in page.
-# signinButton = driver.find_element(:class, "sign-in-button")
-# signinButton.click
-#
-# #wait until the sign in page displays.
-# wait = Selenium::WebDriver::Wait.new(:timeout => 5) # seconds
-# wait.until {driver.find_element(:class => "sign-up-box") }
-#
-# #define user name field element.
-# signupName = driver.find_element(:class, "sign-up-name-input")
-# signupName.send_keys('')
-#
-# #define user email field element.
-# signupEmail = driver.find_element(:class, "sign-up-email-input")
-# signupEmail.send_keys('test@test.com')
-#
-# #define user password field element.
-# signupPassword = driver.find_element(:class, "sign-up-password-input")
-# signupPassword.send_keys('password')
-#
-# #define user password confirmation field element.
-# signupPasswordConfirmation = driver.find_element(:class, "sign-up-password-confirmation-input")
-# signupPasswordConfirmation.send_keys('password')
-#
-# #click Sign up submit button
-# signupSubmitButton = driver.find_element(:link_text, "Sign up")
-# signupSubmitButton.click
-#
-# #wait until the error message is displayed.
-# wait = Selenium::WebDriver::Wait.new(:timeout => 2) # seconds
-# wait.until {driver.find_element(:tag_name, "alert") }
-#
-#
-# ##SIGNUP WITH BAD EMAIL ######################
-# #define user name field element.
-# signupName = driver.find_element(:class, "sign-up-name-input")
-# signupName.send_keys('Test User')
-#
-# #define user email field element.
-# signupEmail = driver.find_element(:class, "sign-up-email-input")
-# signupEmail.send_keys('dd')
-#
-# #define user password field element.
-# signupPassword = driver.find_element(:class, "sign-up-password-input")
-# signupPassword.send_keys('password')
-#
-# #define user password confirmation field element.
-# signupPasswordConfirmation = driver.find_element(:class, "sign-up-password-confirmation-input")
-# signupPasswordConfirmation.send_keys('password')
-#
-# #click Sign up submit button
-# signupSubmitButton = driver.find_element(:link_text, "Sign up")
-# signupSubmitButton.click
-#
-# #wait until the error message is displayed.
-# wait = Selenium::WebDriver::Wait.new(:timeout => 2) # seconds
-# wait.until {driver.find_element(:tag_name, "alert") }
-#
-# ##SIGNUP WITH BAD PASSWORD ######################
-# #define user name field element.
-# signupName = driver.find_element(:class, "sign-up-name-input")
-# signupName.send_keys('Test User')
-#
-# #define user email field element.
-# signupEmail = driver.find_element(:class, "sign-up-email-input")
-# signupEmail.send_keys('test@test.com')
-#
-# #define user password field element.
-# signupPassword = driver.find_element(:class, "sign-up-password-input")
-# signupPassword.send_keys('wrong')
-#
-# #define user password confirmation field element.
-# signupPasswordConfirmation = driver.find_element(:class, "sign-up-password-confirmation-input")
-# signupPasswordConfirmation.send_keys('password')
-#
-# #click Sign up submit button
-# signupSubmitButton = driver.find_element(:link_text, "Sign up")
-# signupSubmitButton.click
-#
-# #wait until the error message is displayed.
-# wait = Selenium::WebDriver::Wait.new(:timeout => 2) # seconds
-# wait.until {driver.find_element(:tag_name, "alert") }
-#
-# ##SUCCESSFUL SIGNUP ######################
-# #define user name field element.
-# signupName = driver.find_element(:class, "sign-up-name-input")
-# signupName.send_keys('Test User')
-#
-# #define user email field element.
-# signupEmail = driver.find_element(:class, "sign-up-email-input")
-# signupEmail.send_keys('test@test.com')
-#
-# #define user password field element.
-# signupPassword = driver.find_element(:class, "sign-up-password-input")
-# signupPassword.send_keys('password')
-#
-# #define user password confirmation field element.
-# signupPasswordConfirmation = driver.find_element(:class, "sign-up-password-confirmation-input")
-# signupPasswordConfirmation.send_keys('password')
-#
-# #click Sign up submit button
-# signupSubmitButton = driver.find_element(:link_text, "Sign up")
-# signupSubmitButton.click
-#
-# #wait until the main welcome page displays.
-# wait = Selenium::WebDriver::Wait.new(:timeout => 5) # seconds
-# wait.until {driver.find_element(:link_text => "Sign Out") }
-#
-# logoutButton = driver.find_element(:link_text, "Sign Out")
-# logoutButton.click
-#
-# puts "Test Passed: Sign up for Standard User validated."
-#
-# # Drop browser object
-# driver.quit
+require 'selenium-webdriver'
+require 'watir'
+
+driver = Selenium::WebDriver.for :firefox
+
+Given ("I navigate to homepage using Firefox browser") do
+  wait = Selenium::WebDriver::Wait.new(:timeout => 2)
+  wait.until{driver.get 'http://localhost:3000'}
+end
+
+And("I click on Login button and wait for the log in page to display") do
+  signinButton = driver.find_elements(:class, "sign-in-button")
+  signinButton.click
+  wait = Selenium::WebDriver::Wait.new(:timeout => 2) # seconds
+  wait.until {driver.find_elementss(:class => 'sign-up-box') }
+end
+
+When("When I set the username empty") do
+  signupName = driver.find_elements(:class, "sign-up-name-input")
+  signupName.send_keys('')
+end
+
+When("When I set the user email {string}") do
+  signupEmail = driver.find_elements(:class, "sign-up-email-input")
+  signupEmail.send_keys('test@test.com')
+end
+
+When("When I set the user password to {string}") do
+  signupPassword = driver.find_elements(:class, "sign-up-password-input")
+  signupPassword.send_keys('password')
+end
+
+When("When I set the user password confirmation to {string}") do
+  signupPasswordConfirmation = driver.find_element(:class, "sign-up-password-confirmation-input")
+  signupPasswordConfirmation.send_keys('password')
+end
+
+And("And I click on sign up button") do
+  signupSubmitButton = driver.find_element(:link_text, "Sign up")
+  signupSubmitButton.click
+end
+
+Then("Then I see the error message") do
+  wait = Selenium::WebDriver::Wait.new(:timeout => 2) # seconds
+  wait.until {driver.find_element(:tag_name, "alert") }
+end
+
+
+##SIGNUP WITH BAD EMAIL ######################
+
+Given ("I am already on sign up page") do
+  wait = Selenium::WebDriver::Wait.new(:timeout => 2)
+  wait.until{driver.find_elements(:class => 'sign-up-box')}
+end
+
+When("When I set the username to {string}") do
+  signupName = driver.find_elements(:class, "sign-up-name-input")
+  signupName.send_keys('Test User')
+end
+
+When("When I set the user email {string}") do
+  signupEmail = driver.find_elements(:class, "sign-up-email-input")
+  signupEmail.send_keys('dd')
+end
+
+When("When I set the user password to {string}") do
+  signupPassword = driver.find_elements(:class, "sign-up-password-input")
+  signupPassword.send_keys('password')
+end
+
+When("When I set the user password confirmation to {string}") do
+  signupPasswordConfirmation = driver.find_elements(:class, "sign-up-password-confirmation-input")
+  signupPasswordConfirmation.send_keys('password')
+end
+
+And("And I click on sign up button") do
+  signupSubmitButton = driver.find_element(:link_text, "Sign up")
+  signupSubmitButton.click
+end
+
+Then("Then I see the error message") do
+  wait = Selenium::WebDriver::Wait.new(:timeout => 2) # seconds
+  wait.until {driver.find_element(:tag_name, "alert") }
+end
+
+
+##SIGNUP WITH BAD PASSWORD ######################
+
+Given ("I am already on sign up page") do
+  wait = Selenium::WebDriver::Wait.new(:timeout => 2)
+  wait.until{driver.find_elementss(:class => 'sign-up-box')}
+end
+
+When("When I set the username to {string}") do
+  signupName = driver.find_elements(:class, "sign-up-name-input")
+  signupName.send_keys('Test User')
+end
+
+When("When I set the user email {string}") do
+  signupEmail = driver.find_elements(:class, "sign-up-email-input")
+  signupEmail.send_keys('test@test.com')
+end
+
+When("When I set the user password to {string}") do
+  signupPassword = driver.find_elements(:class, "sign-up-password-input")
+  signupPassword.send_keys('wrong')
+end
+
+When("When I set the user password confirmation to {string}") do
+  signupPasswordConfirmation = driver.find_elements(:class, "sign-up-password-confirmation-input")
+  signupPasswordConfirmation.send_keys('wrong')
+end
+
+And("And I click on sign up button") do
+  signupSubmitButton = driver.find_element(:link_text, "Sign up")
+  signupSubmitButton.click
+end
+
+Then("Then I see the error message") do
+  wait = Selenium::WebDriver::Wait.new(:timeout => 2) # seconds
+  wait.until {driver.find_element(:tag_name, "alert") }
+end
+
+
+##SUCCESSFUL SIGNUP ######################
+
+Given ("I am already on sign up page") do
+  wait = Selenium::WebDriver::Wait.new(:timeout => 2)
+  wait.until{driver.find_elementss(:class => 'sign-up-box')}
+end
+
+When("When I set the username to {string}") do
+  signupName = driver.find_elements(:class, "sign-up-name-input")
+  signupName.send_keys('Test User')
+end
+
+When("When I set the user email {string}") do
+  signupEmail = driver.find_elements(:class, "sign-up-email-input")
+  signupEmail.send_keys('test@test.com')
+end
+
+When("When I set the user password to {string}") do
+  signupPassword = driver.find_elements(:class, "sign-up-password-input")
+  signupPassword.send_keys('password')
+end
+
+When("When I set the user password confirmation to {string}") do
+  signupPasswordConfirmation = driver.find_elements(:class, "sign-up-password-confirmation-input")
+  signupPasswordConfirmation.send_keys('password')
+end
+
+And("And I click on sign up button") do
+  signupSubmitButton = driver.find_element(:link_text, "Sign up")
+  signupSubmitButton.click
+end
+
+Then("I am on the homepage and I log out") do
+  logoutButton = driver.find_element(:link_text, "Sign Out")
+  logoutButton.click
+end
+
+puts "Test Passed: Sign up for Standard User validated."
+
+# Drop browser object
+driver.quit
